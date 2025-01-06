29a83407024dc901f6db37e5065b98a4198a4aca
Ernest-Yoyowah <ernestniiyoyowah@gmail.com>
2025-01-06 20:14:35 +0000

    adding featured brands

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
-import Navbar from "@/components/Navbar";
-import Featured from "@/components/Featured";
+import Navbar from "@/common/ui/navbar";
+import Featured from "@/features/featured";
 
 export default function Home() {
   return (
