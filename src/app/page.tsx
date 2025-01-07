a486595c5656cf22696c2da299bba22232bbd489
Ernest-Yoyowah <ernestniiyoyowah@gmail.com>
2025-01-07 21:05:27 +0000

    adding brand media

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
+import Footer from "@/common/ui/footer";
 import Navbar from "@/common/ui/navbar";
+import About from "@/features/about";
 import Featured from "@/features/featured";
+import FeaturedPlugins from "@/features/featured/components/featuredPlugins";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Featured />
+      <FeaturedPlugins />
+      <About />
+      <Footer />
     </>
   );
 }
